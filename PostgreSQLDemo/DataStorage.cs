0aa63a6d064ddbb7baf81e6f99325d5d97bd9f35
王玮 <306312851@qq.com>
2019-12-10 19:03:09 +0800

    PostgreSqlDemo数据库

---
+++ PostgreSQLDemo/DataStorage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Data;
+namespace DB
+{
+    public class DataStorage
+    {
+        Npgsql.NpgsqlConnection conn = null;
+        public DataStorage() {
+            conn = new Npgsql.NpgsqlConnection("Server=127.0.0.1;Port=5432;UserId=postgres;Password=123;Database=TestDb;");            
+            conn.Open();
+            
+        }
+        ~DataStorage() {
+            conn.Close();
+        }
+        /// <summary>
+        /// 执行sql update，insert等语句，返回影响行数
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public int ExecuteNonQuery(string sql) {
+            Npgsql.NpgsqlCommand command = new Npgsql.NpgsqlCommand(sql, conn);
+            int rowsaffected = -1;
+            try
+            {
+                rowsaffected = command.ExecuteNonQuery();
+                Console.WriteLine("影响行数：{0}", rowsaffected);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("插入数据库失败" + ex.Message);
+            }
+            return rowsaffected;
+        }
+        /// <summary>
+        /// 查询一个字段
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public object ExecuteScalar(string sql) {
+            object obj = null;
+            Npgsql.NpgsqlCommand command = new Npgsql.NpgsqlCommand(sql, conn);
+            try
+            {
+                obj = command.ExecuteScalar();
+            }
+            catch {
+                Console.WriteLine("查询数据失败");
+            }
+            return obj;
+        }
+        public DataSet Query(string sql) {
+            DataSet ds = new DataSet();
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(sql, conn);
+            try
+            {
+                adapter.Fill(ds);
+            }
+            catch {
+                ds = null;
+            }
+            return ds;
+        }
+    }
+}
---
+++ PostgreSQLDemo/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DB;
+namespace PostgreSQLDemo
+{
+    public partial class Form1 : Form
+    {
+        string sql1 = "insert into \"MyTable\"(id,name) values(2,'wangwei2');";
+        string sql2 = "select count(*) from \"MyTable\";";
+        string sql3 = "select * from \"MyTable\";";
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
+        [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        [System.Runtime.InteropServices.DllImport("Kernel32")]
+        public static extern void FreeConsole();
+
+        DB.DataStorage db = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AllocConsole();
+            db = new DataStorage();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = db.ExecuteNonQuery(sql1);
+            Console.WriteLine("button1_Click count = {0}", count);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            object obj = db.ExecuteScalar(sql2);
+            Console.WriteLine("行数：{0}", Convert.ToInt32(obj));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataSet ds = db.Query(sql3);
+            DataTable tb = ds.Tables[0];
+            foreach (DataRow row in tb.Rows) {
+                string str = "";
+                for (int i = 0; i < tb.Columns.Count; ++i) {
+                    //Console.Write(row[i]);
+                    str += row[i].ToString();
+                    if (i + 1 != tb.Columns.Count) {
+                        //Console.Write(",");
+                        str += ",";
+                    }
+                }
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
