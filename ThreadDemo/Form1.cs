ba81fdbbbc4733ef94835858cfd2555d3d2c05ef
王玮 <306312851@qq.com>
2019-12-10 10:00:49 +0800

    1

---
+++ ThreadDemo/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace ThreadDemo
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
+        [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        [System.Runtime.InteropServices.DllImport("Kernel32")]
+        public static extern void FreeConsole();
+
+        Thread thread;
+        Test t1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t1 = new Test();
+            t1.myAction = CallBack;
+            thread = new Thread(new ThreadStart(t1.run));
+            thread.Start();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t1 = null;
+            thread.Abort();
+            thread.Join();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AllocConsole();
+        }
+        void CallBack(string str) {
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ ThreadDemo/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace ThreadDemo
+{
+    public class Test
+    {
+        public Test() {
+            Name = "这是一个名字";
+        }
+        ~Test() {
+            Console.WriteLine("Test被析构");
+        }
+        public void run()
+        {
+            int count = 0;
+            lock (this)
+            {
+                try {
+                    while (true) {
+                        Thread.Sleep(1000);
+                        //Console.WriteLine(Name);
+                        string tmp = Name + count;
+                        if (myAction != null)
+                        {
+                            myAction(tmp);
+                        }
+                    }
+                } catch {
+                    Console.WriteLine("退出线程");
+                }
+            }
+        }
+        private string Name;
+        public Action<string> myAction = null;
+    }
+}
